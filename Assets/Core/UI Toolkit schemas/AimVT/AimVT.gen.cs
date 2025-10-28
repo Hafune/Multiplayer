4a6b9aae225615544885be9bc34f02f75da187b8
Hafune <hafu985@gmail.com>
2025-10-28 18:04:38 +0500

    ls

---
+++ Assets/Core/UI Toolkit schemas/AimVT/AimVT.gen.cs
@@ -0,0 +1,18 @@
+// -----------------------
+// script auto-generated
+// any changes to this file will be lost on next code generation
+// com.quickeye.ui-toolkit-plus ver: 3.0.3
+// -----------------------
+using UnityEngine.UIElements;
+
+public class AimVT
+{
+    
+    
+    public VisualElement point;
+    
+    public AimVT(VisualElement root)
+    {
+        point = root.Q<VisualElement>("Point");
+    }
+}
